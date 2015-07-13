c714b04e81892cfed0c7580d1c1d7f4f062d648b
alterJK <vitalslp95@mail.ru>
2015-07-13 15:25:30 +0300

    добавлен класс создания "яблока", увеличение длины змейки с каждым съеденным яблоком. учтена возможность "самоедства". ведется подсчет каждого съеденного яблока. если это число превышает количество съеденных яблок при предыдущих попытках, то оно сохраняется как рекорд

--- snake.py
+++ snake.py
@@ -1,21 +1,34 @@
-from tkinter import * # подключение графической библиотеки
+from tkinter import *
+
+import random
 
 #глобальные перменные
 WIDTH = 500 #ширина окна
 HEIGHT = 500 #высота окна
 PART_SIZE = 25 #размер части змейки
-FL_GM = True #флаг состаяния игры 
+FL_GM = True #флаг состаяния игры
+record = 0
+rec = 0
 
+#------------------------------------------------------------------------------------
+#создание "яблока"
+def create_block():
+    global BLOCK
+    posx = PART_SIZE * random.randint(1, (WIDTH-PART_SIZE) / PART_SIZE)
+    posy = PART_SIZE * random.randint(1, (HEIGHT-PART_SIZE) / PART_SIZE)
+     
+    # блок это яблоко красного цвета
+    BLOCK = cs.create_oval(posx, posy, posx + PART_SIZE, posy + PART_SIZE,
+                          fill="red")
 #------------------------------------------------------------------------------------
 #основная функция управления игры
 def main():
     global FL_GM
-
+    global rec
+    global record
     #проверка состояния игры
     if FL_GM:
-        #змека начинает свое движение
         s.move()
-
         #определение начала змейки
         head = cs.coords(s.parts[-1].instance)
         x1, y1, x2, y2 = head
@@ -23,7 +36,20 @@ def main():
         #проверка на столкновение змейки со стеной
         if x2 > WIDTH or x1 < 0 or y1 < 0 or y2 > HEIGHT:
             FL_GM = False
-
+ 
+        # Поедание яблок 
+        if head == cs.coords(BLOCK):
+            rec=int(rec+1)
+            s.add_segment()
+            cs.delete(BLOCK)
+            create_block()
+ 
+        # Самоедство
+        else:
+            # Проходим по всем сегментам змеи
+            for index in range(len(s.parts)-1):
+                if cs.coords(s.parts[index].instance) == head:
+                    FL_GM = False
             
         consol.after(100, main)
     #сообщение об окончании игры
@@ -33,8 +59,26 @@ def main():
                       font="Colibri 50",
                       fill="red")
 
-#------------------------------------------------------------------------------------
-#создание "яблока"
+        cs.create_text(WIDTH/2, HEIGHT/2+50,
+                      text="Score %s" % (rec),
+                      font="Colibri 20",
+                      fill="yellow")
+
+        #f = open('records.txt','w')
+        #f.write(str(0))
+        #f.close()
+
+        f = open('records.txt','r')
+        line = f.readline()
+        record = int(line)
+        f.close()
+
+        f = open('records.txt','w')
+        if rec > record:
+            f.write(str(rec))
+        else:
+            f.write(str(record))
+        f.close()
 
 #------------------------------------------------------------------------------------
 #класс части змейки
@@ -42,7 +86,7 @@ class Part(object):
     #метод создания части змейки заданного размера (PART_SIZE), белого цвета
     def __init__(a, x, y):
         a.instance = cs.create_rectangle(x, y, x+PART_SIZE, y+PART_SIZE, fill="white")
-
+#------------------------------------------------------------------------------------
 #класс змейки
 class Snake(object):
     #метод создания змейки
@@ -80,7 +124,14 @@ class Snake(object):
         if event.keysym in a.mapping:
             a.vector = a.mapping[event.keysym] #меняем направление
 
-
+    #увеличение длины змейки
+    def add_segment(a):
+        last_seg = cs.coords(a.parts[0].instance)
+        x = last_seg[2] - PART_SIZE
+        y = last_seg[3] - PART_SIZE
+        a.parts.insert(0, Part(x, y))
+        
+ 
 #------------------------------------------------------------------------------------
 #создание окна
 consol = Tk()
@@ -95,14 +146,18 @@ cs.grid()
 #наведение фокуса на Canvas для определения нажатия на кнопку
 cs.focus_set()
 
-#создание частей змейки (ореинтация змейки - горизонтальная)
-parts = [Part(PART_SIZE, PART_SIZE), Part(PART_SIZE*2, PART_SIZE), Part(PART_SIZE*3, PART_SIZE)]
+#создание 3 частей змейки (ориентация змейки - горизонтальная)
+parts = [Part(PART_SIZE, PART_SIZE), Part(PART_SIZE*2, PART_SIZE),
+         Part(PART_SIZE*3, PART_SIZE)]
 
 #создание "змейки" с помощью созданных выше ее частей
 s = Snake(parts)
-
+rec = 0
 #реакция на нажатие кнопки
 cs.bind("<KeyPress>", s.change_direction)
+
+#создать "яблоко"
+create_block()
 
 #выполнить основную функцию игры
 main()
