aa788260feb2af55a0ea04e23a42e1563aebf1d5
alterJK <vitalslp95@mail.ru>
2015-07-13 12:25:26 +0300

    добавлены функции движения змейки, а также проверка на столкновение с границами поля

--- snake.py
+++ snake.py
@@ -8,7 +8,30 @@ FL_GM = True #флаг состаяния игры
 
 #------------------------------------------------------------------------------------
 #основная функция управления игры
-#def main():
+def main():
+    global FL_GM
+
+    #проверка состояния игры
+    if FL_GM:
+        #змека начинает свое движение
+        s.move()
+
+        #определение начала змейки
+        head = cs.coords(s.parts[-1].instance)
+        x1, y1, x2, y2 = head
+        
+        #проверка на столкновение змейки со стеной
+        if x2 > WIDTH or x1 < 0 or y1 < 0 or y2 > HEIGHT:
+            FL_GM = False
+
+            
+        consol.after(100, main)
+    #сообщение об окончании игры
+    else:
+        cs.create_text(WIDTH/2, HEIGHT/3,
+                      text="GAME OVER !",
+                      font="Colibri 50",
+                      fill="red")
 
 #------------------------------------------------------------------------------------
 #создание "яблока"
@@ -27,11 +50,36 @@ class Snake(object):
         a.parts = parts
 
         #доступные змейке направления
-        a.mapping = {"Вправо": (1, 0), "Влево": (-1, 0),"Вниз": (0, 1), "Вверх": (0, -1)}
+        a.mapping = {"Right": (1, 0), "Left": (-1, 0),"Down": (0, 1), "Up": (0, -1)}
         #первоначальное напрвление змейки - вправо
-        a.vector = a.mapping["Вправо"]
+        a.vector = a.mapping["Right"]
 
     #управление змейкой
+    #---------------------------------------------------------------------------
+    #движение змейки
+    def move(a):   
+         #рассмотрим все части змейки, за исключением первой
+         for index in range(len(a.parts)-1):
+              part = a.parts[index].instance
+              x1, y1, x2, y2 = cs.coords(a.parts[index+1].instance)
+              # задаем каждому сегменту позицию сегмента стоящего после него
+              cs.coords(part, x1, y1, x2, y2)
+          
+         # получаем координаты сегмента перед "головой"
+         x1, y1, x2, y2 = cs.coords(a.parts[-2].instance)
+          
+         # помещаем "голову" в направлении указанном в векторе движения
+         cs.coords(a.parts[-1].instance, x1 + a.vector[0]*PART_SIZE,
+                   y1 + a.vector[1]*PART_SIZE, x2 + a.vector[0]*PART_SIZE,
+                   y2 + a.vector[1]*PART_SIZE)
+
+    #изменение направления
+    def change_direction(a, event):
+    #event - событие, обозначающее нажатие на кнопку
+    #проверка кнопки на обозначение направления
+        if event.keysym in a.mapping:
+            a.vector = a.mapping[event.keysym] #меняем направление
+
 
 #------------------------------------------------------------------------------------
 #создание окна
@@ -44,6 +92,8 @@ consol.title("Игра Snake")
 cs = Canvas(consol, width=WIDTH, height=HEIGHT, bg="black")
 #помещаем его в "таблицу ячеек" с помощью упаковщика grid 
 cs.grid()
+#наведение фокуса на Canvas для определения нажатия на кнопку
+cs.focus_set()
 
 #создание частей змейки (ореинтация змейки - горизонтальная)
 parts = [Part(PART_SIZE, PART_SIZE), Part(PART_SIZE*2, PART_SIZE), Part(PART_SIZE*3, PART_SIZE)]
@@ -51,6 +101,11 @@ parts = [Part(PART_SIZE, PART_SIZE), Part(PART_SIZE*2, PART_SIZE), Part(PART_SIZ
 #создание "змейки" с помощью созданных выше ее частей
 s = Snake(parts)
 
+#реакция на нажатие кнопки
+cs.bind("<KeyPress>", s.change_direction)
+
+#выполнить основную функцию игры
+main()
 #------------------------------------------------------------------------
 #запуск окна
 consol.mainloop()
